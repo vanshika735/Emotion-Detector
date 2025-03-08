4f0d9939f268e61c1e66a440e86f57e060c45aa9
vanshika <vanshikasharma7854@gmail.com>
2025-03-09 01:47:34 +0530

    Update factories.py

--- factories.py
+++ factories.py
@@ -1,14 +1,1 @@
-from faker import Faker
 
-fake = Faker()
-
-def fake_product():
-    return {
-        "name": fake.word(),
-        "category": fake.word(),
-        "price": round(fake.random_number(digits=3), 2),
-        "description": fake.sentence(),
-    }
-
-# Print output to see it in the terminal
-print(fake_product())
